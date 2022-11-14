e5bf1b4d637d9046830cc4fe4ea836ab447d5f51
robertolagreca <robertolagreca92@gmail.com>
2022-11-14 17:50:22 +0100

    Snack3 Ver.Base + Bonus

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,18 +2,49 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 
-let arrayNumeriInput = [];
+//BONUS dire chi è il maggiore, minore e media
+
+let arrayInput = [];
 let j = 10;
 let somma = parseInt(0);
 
-for(i = 0; i < j; i++){
-    arrayNumeriInput[i] = prompt("Inserisci il " + (i + 1) + "° numero.");
-    console.log("il " + (i + 1)  + "° numero è " + arrayNumeriInput[i]);
-    somma += parseInt(arrayNumeriInput[i]);
+let maggiore, minore, media;
+mag = 0;
+min = 0;
+media = 0;
+
+for(let i = 0; i < j; i++){
+    arrayInput[i] = prompt("Inserisci il " + (i + 1) + "° numero.");
+    console.log("il " + (i + 1)  + "° numero è " + arrayInput[i]);
+    somma += parseInt(arrayInput[i]);
 }
 
 console.log("La somma dei dieci numeri inseriti è " + somma);
 
+//Bonus Media
+media = somma / j;
+console.log("La media dei 10 numeri è " + media);
+
+//Bonus maggiore e minore
+
+for(let i = 0; i < j; i++){
+    if(parseInt(arrayInput[i]) > parseInt(mag)){
+        mag = arrayInput[i];
+    }
+
+}
+
+min = parseInt(mag);
+
+for(let i = 0; i < j; i++){
+    if(parseInt(arrayInput[i]) < parseInt(min)){
+        min = arrayInput[i];
+    }
+}
+
+
+console.log("Il numero maggiore è " + mag);
+console.log("Il numero minore è " + min);
 
 
 
